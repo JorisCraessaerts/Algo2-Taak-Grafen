c3e03ca9261eb717375745a003605a07b855e53c
JorisCraessaerts <33348068+JorisCraessaerts@users.noreply.github.com>
2023-05-13 13:54:56 +0200

    sorted-edge-list implemented but does not correctly keep track of visited edges

--- a-d/segmentation.rkt
+++ a-d/segmentation.rkt
@@ -15,6 +15,7 @@
     ; Returns a pair containing the number of components resulting from the segmentation (car)
     ; and a node-indexed vector (cdr) mapping the nodes to their corresponding component.
     (define (segment g)
+      (define k 150)
       (define segmentation (initial-segmentation))
 
 
@@ -26,6 +27,26 @@
       (define (initial-segmentation)
         (dset:new (order g)))
 
+
+      ; Code to create a sorted list of edges of a graph
+      (define (n1 edge) (car edge)) ; Abstraction to get node 1 from an edge
+      (define (n2 edge) (cadr edge)) ;Abstraction to get node 2 from an edge
+      (define (weight edge) (caddr edge))
+
+
+      (define (all-edges-list graph)
+        (let ((edges '())
+              (visited (make-vector (order graph) #f)))
+          (for-each-node graph
+                         (lambda (node)
+                           (vector-set! visited node #t)
+                           (for-each-edge graph node
+                                          (lambda (w to)
+                                            (unless (vector-ref visited to)
+                                              (set! edges (cons (list node to w) edges)))))))
+          (sort edges (lambda (edge1 edge2)
+                        (< (weight edge1) (weight edge2))))))
+      
 
       )
 
