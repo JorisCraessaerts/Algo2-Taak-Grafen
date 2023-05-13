7ceb945ef5e5baf40a52951409e06703c7009146
JorisCraessaerts <33348068+JorisCraessaerts@users.noreply.github.com>
2023-05-13 15:39:20 +0200

    sorted-edge-list (hopelijk) nog beter geïmplementeerd

--- a-d/segmentation.rkt
+++ a-d/segmentation.rkt
@@ -32,14 +32,14 @@
       (define (weight edge) (caddr edge)) ; Abstraction to get the weight from an edge
       
       ; Code to create a sorted list of edges of a graph
-      (define (all-edges-vector graph)
+      (define (sorted-edge-list graph)
         (define (edge-has-been-visited visited-edges edge) ; We are working with undirected graphs. This means that every time we see an edge (n1 n2 "some-weight"), we have to check that the same edge (n2 n1 "some-weight") doesn't already exist. I did this by keeping track of a hash table that for every node keeps track of all other nodes it is connected to.
           (let ((n1 (n1 edge))
                 (n2 (n2 edge)))
-            (or (member n2 (hash-ref visited-edges n1 '()))
-                (member n1 (hash-ref visited-edges n2 '())))))
+            (or (member n2 (hash-ref visited-edges n1 '())) ; If n2 is in the list of visited nodes of n1, this means the edge has been visited.
+                (member n1 (hash-ref visited-edges n2 '()))))) ; If n1 is in the list of visited nodes of n2, this means the edge has been visited.
         
-        (let ((edges '#())
+        (let ((edges '())
               (visited-edges (make-hash))) ; make hash is a procedure from racket to create new hashtables. This hashtable is created to keep track of the visited edges (so we are not working with doubles in our undirected graph)
           (for-each-node graph
                          (lambda (from)
@@ -49,15 +49,12 @@
                                               (unless (edge-has-been-visited visited-edges edge)
                                                 (hash-update! visited-edges from (lambda (old) (cons to old)) '()) ; We add the "to-node" of the edge to the list of edges the "from-node" is connected to. (the list in the hash-table for element "from-node"). We do this by consing the "to-node" to the existing list in the hash table
                                                 (hash-update! visited-edges to (lambda (old) (cons from old)) '()) ; We add the "from-node" of the edge to the list of edges the "to-node" is connected to. (the list in the hash-table for element "to-node"). We do this by consing the "from-node" to the existing list in the hash table
-                                                (set! edges (vector-append edges (vector edge))))))))) ; If the edge is not visited yet, we add it to the list of edges that will be sorted.
-          (sort edges (lambda (edge1 edge2)
-                        (< (weight (vector-ref edge1 0)) (weight (vector-ref edge2 0)))))))
-      
-      
-
-      )
+                                                (set! edges (cons edge edges))))))) ; If the edge is not visited yet, we add it to the list of edges that will be sorted.
+                         (sort edges (lambda (edge1 edge2)
+                                       (< (weight edge1) (weight edge2)))))))
 
     
+      )
 
 ))
     
