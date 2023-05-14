a0c82a6e1da5d475a883c2a919d355ee76aac529
JorisCraessaerts <33348068+JorisCraessaerts@users.noreply.github.com>
2023-05-15 00:08:12 +0200

    Enkele functies van plaats gewisseld. Nog wat werken aan scoping

--- a-d/segmentation.rkt
+++ a-d/segmentation.rkt
@@ -5,8 +5,9 @@
   (import (scheme base)
           (racket/hash) ; https://docs.racket-lang.org/reference/hashtables.html
           (a-d graph weighed config) ; I use the adjecency list representation. Pictures can contain millions of pixels but each pixel is only connected to max 8 other pixels surrounding itself. This makes a sparse graph.
-          (prefix (a-d disjoint-sets optimized) dset:)) ; Add any necessary library to implement your assignment here.
-                         ; Only R⁷RS-libraries, sublibraries from the course (a-d) and WPO solutions are allowed! 
+          (prefix (a-d disjoint-sets optimized) dset:))
+  ; Add any necessary library to implement your assignment here.
+  ; Only R⁷RS-libraries, sublibraries from the course (a-d) and WPO solutions are allowed! 
 
   (begin
 
@@ -14,25 +15,19 @@
     ; ( weighted-graph ➙ pair )
     ; Performs the segmentation of a given weighted graph.
     ; Returns a pair containing the number of components resulting from the segmentation (car)
-    ; and a node-indexed vector (cdr) mapping the nodes to their corresponding component.
+    ; and a node-indexed vector (cdr) mapping the nodes to their corresponding component.  
     (define (segment g)
       (define k 150)
       (define n (order g))
-      (define segmentation (initial-segmentation))
-      (define sorted-edges (sorted-edge-list g))
-
-
-      ; Procedure to create the inital segmentation where every node is in its own segment
-      (define (initial-segmentation)
-        (dset:new (order g)))
-
-      ; Abstractions for edges
-      (define (n1 edge) (car edge)) ; Abstraction to get node 1 from an edge
-      (define (n2 edge) (cadr edge)) ; Abstraction to get node 2 from an edge
-      (define (weight edge) (caddr edge)) ; Abstraction to get the weight from an edge
       
-      ; Code to create a sorted list of edges of a graph
-      (define (sorted-edge-list graph)
+        ; Procedure to create the inital segmentation where every node is in its own segment
+      (define (initial-segmentation size)
+        (let ((dsets (dset:new size)))
+          (do ((i 0 (+ i 1))) ((= i size))
+            (hash-set! component-sizes i 1)) ; In de hashtabel gaan we voor elke component de value (de bijhorende size dus) op 1 zetten omdat een component initieel maar uit één node bestaat.
+          dsets))
+
+       (define (sorted-edge-list graph)
         (define (edge-has-been-visited visited-edges edge) ; We are working with undirected graphs. This means that every time we see an edge (n1 n2 "some-weight"), we have to check that the same edge (n2 n1 "some-weight") doesn't already exist. I did this by keeping track of a hash table that for every node keeps track of all other nodes it is connected to.
           (let ((n1 (n1 edge))
                 (n2 (n2 edge)))
@@ -53,8 +48,85 @@
                          (sort edges (lambda (edge1 edge2)
                                        (< (weight edge1) (weight edge2)))))))
 
-    
-      )
+      (define segmentation (initial-segmentation n))
 
-))
+      ; Code to create a sorted list of edges of a graph
+     
+      
+      (define sorted-edges (sorted-edge-list g))
+      (define component-sizes (make-hash)) ; Hash table that keeps tracks of the sizes of the components so we don't have to recalculate the sizes of the components each time we calculate their internal difference.
+      
+      ; Main algorithm logic
+      (for-each
+       (lambda (edge)
+         (let* ((from (n1 edge))
+                (to (n2 edge))
+                (edge-weight (weight edge))
+                (from-component (dset:find segmentation from))
+                (to-component (dset:find segmentation to)))
+           (if (and (not (= from-component to-component)) ; from-component and to-component are different
+                    (< edge-weight (min (component-sum from-component) (component-sum to-component)))) ; edge-weight is smaller than the minimum of (component-sum from-component) and (component-sum to-component)
+               (set! segmentation (merge-components segmentation from-component to-component)))))  
+       sorted-edges)
+
+      (algorithm-result)
+
+      (define (component-vector)
+        (define node-indexed-vector (make-vector n))
+        (for-each
+         (lambda (node)
+           (vector-set! node-indexed-vector node (dset:find segmentation node)))
+         (range n))
+        node-indexed-vector)
+     
+      (define (number-of-components)
+        (define component-set (make-hash))
+        (for-each
+         (lambda (node)
+           (hash-set! component-set (vector-ref node-indexed-vector node) #t))
+         (range n))
+        (hash-count component-set))
+
+      (define (algorithm-result)
+        (cons number-of-components node-indexed-vector))    
+
+      ; Abstractions for edges
+      (define (n1 edge) (car edge)) ; Abstraction to get node 1 from an edge
+      (define (n2 edge) (cadr edge)) ; Abstraction to get node 2 from an edge
+      (define (weight edge) (caddr edge)) ; Abstraction to get the weight from an edge
+     
+
+
+
+      (define (i c)
+        (if (= (hash-ref component-sizes c) 1)
+            0
+            (let ((max-weight 0)) ; We gaan over de nodes van de graaf 
+              (for-each-node g
+                             (lambda (node)
+                               (when (= (dset:find segmentation node) c) ; if node is in component c
+                                 (for-each-edge g node
+                                                (lambda (w to)
+                                                  (when (and (= (dset:find segmentation to) c) ; if "to" node is in component c
+                                                             (> w max-weight)) ; and weight is larger than max so far
+                                                    (set! max-weight w))))))) ; update the max weight
+              max-weight)))
+
+      (define (t c)
+        (let ((component-size (hash-ref component-sizes c)))
+          (/ k component-size)))
+
+      (define (component-sum c)
+        (+ (i c) (t c)))
+
+      ; Code om twee componenten te mergen. Belangrijk hierbij is dat door components te mergen het aantal nodes in deze componenten ook zal veranderen. We moeten dus ook de hashtabel updaten die we gebruiken voor het bijhouden van de sizes van de verschillende componenten.
+     (define (merge-components segmentation c1 c2)
+       (let* ((updated-segmentation (dset:union! segmentation c1 c2))
+              (new-component (dset:find updated-segmentation c1)) ; get the representative of the new component
+              (new-size (+ (hash-ref component-sizes c1 0) (hash-ref component-sizes c2 0))))
+         (hash-remove! component-sizes c1)
+         (hash-remove! component-sizes c2)
+         (hash-set! component-sizes new-component new-size)
+         updated-segmentation))
+)))
     
